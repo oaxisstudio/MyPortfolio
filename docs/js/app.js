638de18ec771eb987cc7110a9105d0e7fe4c8fd2
Ukatsu <greenworld39+prog@gmail.com>
2019-08-20 17:50:18 +0900

    add works-trace.html

--- docs/js/app.js
+++ docs/js/app.js
@@ -10,4 +10,13 @@ $(function () {
     $('.js-toggle-sp-menu-target').toggleClass('active');
   });
 
+  // スムーススクロール
+  $('a[href^="#"]').click(function () {
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
 });
